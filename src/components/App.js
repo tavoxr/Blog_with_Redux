8d14e9a154abe444f994e1881d5174fa7f79df11
Gustavo Herrera <tavoxr23@gmail.com>
2019-11-24 10:12:43 -0600

    creando tabla

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+
+
+const App = (props) => {
+  return(
+    <div className='margen'>
+    <table className='tabla'>
+      <thead>
+        <tr>
+          <th>
+            Nombre
+          </th>
+          <th>
+            Correo
+          </th>
+          <th>
+            Enlace
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            Rodolfo
+          </td>
+          <td>
+            Tavoxr23@gmail.com
+          </td>
+          <td>
+            tavoxr.com
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Lourdes
+          </td>
+          <td>
+            lou@gmail.com
+          </td>
+          <td>
+            Lou.com
+          </td>
+        </tr>
+      </tbody>
+      
+    </table>
+    </div>
+  );
+
+
+
+
+
+
+}
+
+
+export default App;
