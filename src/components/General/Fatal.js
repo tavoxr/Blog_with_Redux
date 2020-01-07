a535dd99cd96b4a7ebc1c341d50d17f3fc557989
Gustavo Herrera <tavoxr23@gmail.com>
2020-01-06 23:49:37 -0600

    component Tabla

---
+++ src/components/General/Fatal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Fatal = (props)=>(
+    <div className='center'>
+        <h2 className='rojo'>
+           {props.mensaje}
+
+        </h2>
+    </div>
+)
+
+export default Fatal;
--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
+import Tabla from './Tabla';
+
+
 class Usuarios extends React.Component {
 
   
@@ -16,47 +20,14 @@ class Usuarios extends React.Component {
       return <Spinner/>;
     }
 
-     return(
-      <table className='tabla'>
-        <thead>
-          <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.ponerFilas()}
-        </tbody>
-       </table>
-     )
+    if(this.props.error){
+      return <Fatal mensaje={this.props.error}/>;
+    }
+
+     return <Tabla  />;
    }
 
 
-   ponerFilas=()=>(
-     this.props.usuarios.map((usuario)=>{
-              
-       return( 
-          <tr key={ usuario.id}>
-            <td>
-              {usuario.name}
-            </td>
-            <td>
-              {usuario.email}
-            </td>
-            <td>
-              {usuario.website}
-            </td>
-          </tr>
-          
-          ) })
-   )
 
 
 
@@ -70,6 +41,7 @@ console.log(this.props.error);
 
     return(
       <div className='margen'>
+        <h1> Usuarios </h1>
         {this.ponerContenido()}
       </div>
     );
